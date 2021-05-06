3b508b81ce857e851ba7ef3c9bed8cfef7c70d93
lorenperalta <stinpc@hotmail.com>
2021-05-06 18:20:41 -0500

    maven structure

---
+++ mavendeploy/src/main/java/maven/deploy/mavendeploy/MavendeployApplication.java
@@ -0,0 +1,13 @@
+package maven.deploy.mavendeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavendeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavendeployApplication.class, args);
+	}
+
+}
---
+++ mavendeploy/src/test/java/maven/deploy/mavendeploy/MavendeployApplicationTests.java
@@ -0,0 +1,13 @@
+package maven.deploy.mavendeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavendeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
